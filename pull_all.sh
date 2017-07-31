87e17215bf1533c2724f207c858c4b96480a18b4
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-07-31 22:40:05 +0000

    add script to easily pull all images locally

---
+++ pull_all.sh
@@ -0,0 +1,5 @@
+docker pull ospray/docker-images:arch
+docker pull ospray/docker-images:centos6
+docker pull ospray/docker-images:centos7
+docker pull ospray/docker-images:ubuntu14.04
+docker pull ospray/docker-images:ubuntu16.04
