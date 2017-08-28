6e8c4ab48b10cfca2d3a9e699b853d46f5b7f9e7
Johannes Günther <johannes.guenther@intel.com>
2017-08-28 10:11:47 +0200

    Pull all images

--- pull_all.sh
+++ pull_all.sh
@@ -1,6 +1,1 @@
-docker pull ospray/docker-images:arch
-docker pull ospray/docker-images:docu
-docker pull ospray/docker-images:centos6
-docker pull ospray/docker-images:centos7
-docker pull ospray/docker-images:ubuntu14.04
-docker pull ospray/docker-images:ubuntu16.04
+docker pull -a ospray/docker-images
